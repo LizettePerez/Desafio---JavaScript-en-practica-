eabc5777a65172ecb4132d9321b8ccfc00ae2bde
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-04-27 20:29:35 -0400

    update test 2

--- Ejercicio N°2/assets/js/logica.js
+++ Ejercicio N°2/assets/js/logica.js
@@ -1,31 +1,31 @@
-let pintar = document.querySelector('.pintar');
-let rojo = document.querySelector('.rojo');
-let naranja = document.querySelector('.naranja');
-let amarillo = document.querySelector('.amarillo');
-let verde = document.querySelector('.verde');
-let aqua = document.querySelector('.aqua');
-let rosa = document.querySelector('.rosa');
+let pintar = document.querySelector('#caja');
+let rojo = document.querySelector('#btn-1');
+let naranja = document.querySelector('#btn-2');
+let amarillo = document.querySelector('#btn-3');
+let verde = document.querySelector('#btn-4');
+let aqua = document.querySelector('#btn-5');
+let rosa = document.querySelector('#btn-6');
 
 rojo.addEventListener('click', () => {
-  pintar.style.backgroundColor = 'red';
+  pintar.style.backgroundColor = '#e53e3e';
 });
 
 naranja.addEventListener('click', () => {
-  pintar.style.backgroundColor = 'orange';
+  pintar.style.backgroundColor = '#dd6b20';
 });
 
 amarillo.addEventListener('click', () => {
-  pintar.style.backgroundColor = 'yellow';
+  pintar.style.backgroundColor = '#faf089';
 });
 
 verde.addEventListener('click', () => {
-  pintar.style.backgroundColor = 'green';
+  pintar.style.backgroundColor = '#48bb78';
 });
 
 aqua.addEventListener('click', () => {
-  pintar.style.backgroundColor = 'aqua';
+  pintar.style.backgroundColor = '#81e6d9';
 });
 
 rosa.addEventListener('click', () => {
-  pintar.style.backgroundColor = 'pink';
+  pintar.style.backgroundColor = '#d53f8c';
 });
