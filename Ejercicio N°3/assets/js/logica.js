573c293369ab94b4eb164e84de4e4d2eb2e12ebe
Lizette Pérez <lizette.perezcorrea@gmail.com>
2023-04-27 20:23:46 -0400

    update test 3

--- Ejercicio N°3/assets/js/logica.js
+++ Ejercicio N°3/assets/js/logica.js
@@ -1,6 +1,6 @@
-const numero1 = document.getElementById('num1');
-const numero2 = document.getElementById('num2');
-const resultado = document.getElementById('resultado');
+const numero1 = document.getElementById('valor1');
+const numero2 = document.getElementById('valor2');
+const resultado = document.querySelector('.resultado');
 
 const sumar = () => {
   const n1 = parseInt(numero1.value);
@@ -8,9 +8,9 @@ const sumar = () => {
 
   if (isNaN(n1) || isNaN(n2)) {
     alert("Debe ingresar un número");
-    resultado.textContent = "";
+    resultado.innerHTML = "";
   } else {
-    resultado.textContent = `La suma es ${n1 + n2}`
+    resultado.innerHTML = ` ${n1 + n2} `
   }
 
 }
@@ -20,17 +20,17 @@ const restar = () => {
   const n2 = parseInt(numero2.value);
   if (isNaN(n1) || isNaN(n2)) {
     alert("Debe ingresar un número");
-    resultado.textContent = "";
+    resultado.innerHTML = "";
   } else if (n1 < n2) {
-    resultado.textContent = `La resta es 0`
+    resultado.innerHTML = ` 0 `
   } else {
-    resultado.textContent = `La resta es ${n1 - n2}`
+    resultado.innerHTML = ` ${n1 - n2} `
   }
 }
 
 
-const botonSumar = document.getElementById('sumar');
+const botonSumar = document.getElementById('btn-sumar');
 botonSumar.addEventListener('click', sumar)
 
-const botonRestar = document.getElementById('restar');
+const botonRestar = document.getElementById('btn-restar');
 botonRestar.addEventListener('click', restar)
